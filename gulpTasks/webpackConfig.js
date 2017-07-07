5ef4735010030a9bce1c4d173b81a30a466f10b7
pepemiyashiro <pepe.miyashiro@beauthentic.digital>
2017-07-07 12:45:46 -0500

    [feature/config] base config

--- gulpTasks/webpackConfig.js
+++ gulpTasks/webpackConfig.js
@@ -1,18 +1,20 @@
 import path from 'path';
 import process from 'process';
 
+import { pathTo } from './config'
+
+const resolve = path.resolve;
 const isProduction = (process.env.NODE_ENV === 'production');
 
 // NOTE: Config must be in a separate file
 const webpackConfig = {
-  entry: '../source/main.js',
+  entry: resolve(__dirname, '..', pathTo.src, 'main.js'),
   output: {
     filename: './js/global.js',
-    path: path.resolve(__dirname, '../dist')
+    path: resolve(__dirname, '..', pathTo.dist)
   },
-  context: path.resolve(__dirname, '../dist'),
+  context: resolve(__dirname, '..', pathTo.dist),
   plugins: isProduction ? [new webpack.optimize.UglifyJsPlugin()] : []
 }
-
 
 export { webpackConfig }
